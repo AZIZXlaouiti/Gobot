fe7579f38eb75cc255975a6adc5aa7bafe7da07b
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-31 00:44:44 -0500

    adding the bot to the slack channel

--- bot.py
+++ bot.py
@@ -8,3 +8,7 @@ load_dotenv(dotenv_path = env_path)
 
 client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
 print(os.environ['SLACK_TOKEN'])
+client.chat_postMessage(
+    channel='#random',
+    text="hello i'm a bot ask me anything"
+    )
