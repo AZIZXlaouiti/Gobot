03757b8add381537b10c0089ffc58120b431c97c
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-31 16:10:20 -0500

    adding flask and using ngrok hosting

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
-import slack 
 import os 
+from slack import WebClient
 from dotenv import load_dotenv
 from pathlib import Path 
-# print(os.environ.get("USER"))
+from slackeventsapi import SlackEventAdapter
+from flask import Flask
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path = env_path)
 
-client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
-print(os.environ['SLACK_TOKEN'])
-client.chat_postMessage(
-    channel='#random',
-    text="hello i'm a bot ask me anything"
-    )
+app = Flask(__name__)
+
+# adding a slack endpoint
+slack_adapter = SlackEventAdapter(os.environ['SIGN_SECRET'],'/slack/events',app)
+
+client = WebClient(token = os.environ['SLACK_TOKEN'])
+
+
+print("ahla bik")
+# client.chat_postMessage(
+#     channel='#random',
+#     text="happy new year!!"
+#     )
+if __name__ == "__main__":
+    app.run(debug=True , port=5000 )    
---
+++ script.py
@@ -0,0 +1,11 @@
+import sys 
+
+print(sys.version)
+print(sys.executable)
+
+def greet(whio_to_greet):
+    greeting = f'hello, {whio_to_greet}'
+    return greeting
+
+print(greet("winek a zebi !!"))
+
