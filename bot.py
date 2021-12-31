37c1cb0ce44c8e03995373f29fcab5933fe95870
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-31 00:15:42 -0500

    adding dotenv var and using slackclient

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
-# import slack 
+import slack 
 import os 
+from dotenv import load_dotenv
 from pathlib import Path 
-# from dotenv import load_dotenv
-# print("hello")
+# print(os.environ.get("USER"))
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path = env_path)
+
+client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
+print(os.environ['SLACK_TOKEN'])
